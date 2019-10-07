36a89f866ea761894b822d98aadb4452186cdb76
Katarzyna <katarzyna.panek111@gmail.com>
2019-10-07 11:38:14 +0200

    Poprawiłam metodę isRightTriangle.

--- src/TriangleMethode.java
+++ src/TriangleMethode.java
@@ -1,7 +1,9 @@
 public class TriangleMethode {
 
     boolean isRightTriangle(Triangle triangle) {
-        boolean b = triangle.lenghtA * triangle.lenghtA * triangle.lenghtB * triangle.lenghtB == triangle.lenghtC * triangle.lenghtC;
+        boolean b = ((triangle.lenghtA * triangle.lenghtA * triangle.lenghtB * triangle.lenghtB) == (triangle.lenghtC * triangle.lenghtC))
+                || ((triangle.lenghtA * triangle.lenghtA * triangle.lenghtC * triangle.lenghtC) == (triangle.lenghtB * triangle.lenghtB))
+                || ((triangle.lenghtB * triangle.lenghtB * triangle.lenghtC * triangle.lenghtC) == (triangle.lenghtA * triangle.lenghtA));
         return b;
     }
 }
